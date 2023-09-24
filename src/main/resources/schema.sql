ff3800e32307740c6d0a1f247bcbbb8d1fce0024
MattDog222 <wilder.matthew.20@gmail.com>
2023-09-24 13:35:41 -0500

    Add Initial SQL Scripts
    schema + some data

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,50 @@
+CREATE DATABASE IF NOT EXISTS clan_capital;
+
+USE clan_capital;
+
+-- All districts
+CREATE TABLE IF NOT EXISTS `district_enum`
+(
+	`id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL UNIQUE
+);
+
+-- All buildings
+CREATE TABLE IF NOT EXISTS `building_enum`
+(
+	`id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(100) NOT NULL UNIQUE
+);
+
+-- All building upgrades with level and cost
+CREATE TABLE IF NOT EXISTS `building_upgrade`
+(
+	`id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	`building_enum_id` INT NOT NULL,
+	`level` INT NOT NULL,
+	`cost` INT NOT NULL DEFAULT 0,
+	UNIQUE (`building_enum_id`, `level`),
+	FOREIGN KEY (`building_enum_id`) REFERENCES `building_enum`(`id`) ON DELETE NO ACTION
+);
+
+-- Each building instance gets a current state represented by a level
+CREATE TABLE IF NOT EXISTS `building_instance`
+(
+	`id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT, -- Which specific building instance
+	`district_enum_id` INT NOT NULL, -- What district
+    `building_enum_id` INT NOT NULL, -- What building
+	`level` INT NOT NULL, -- What level
+	FOREIGN KEY (`district_enum_id`) REFERENCES `district_enum`(`id`) ON DELETE NO ACTION,
+	FOREIGN KEY (`building_enum_id`) REFERENCES `building_enum`(`id`) ON DELETE NO ACTION
+);
+
+-- How many of each building can be built in a district
+CREATE TABLE IF NOT EXISTS `district_building_limit`
+(
+	`district_enum_id` INT NOT NULL,
+	`building_enum_id` INT NOT NULL,
+	`maximum_amount` INT NOT NULL DEFAULT 0,
+	UNIQUE KEY (`district_enum_id`,`building_enum_id`),
+	FOREIGN KEY (`district_enum_id`) REFERENCES `district_enum`(`id`) ON DELETE NO ACTION,
+	FOREIGN KEY (`building_enum_id`) REFERENCES `building_enum`(`id`) ON DELETE NO ACTION
+);
