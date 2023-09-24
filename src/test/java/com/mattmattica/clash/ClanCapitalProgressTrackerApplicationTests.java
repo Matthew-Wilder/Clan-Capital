888ca5baecde737436aa76dc15ae38ce687a62b9
MattDog222 <wilder.matthew.20@gmail.com>
2023-09-24 13:34:11 -0500

    Spring Initializr
    https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.1.4&packaging=jar&jvmVersion=17&groupId=com.mattmattica&artifactId=clash&name=Clan%20Capital%20Progress%20Tracker&description=Progress%20Tracking%20for%20Clan%20Capital%20in%20Clash%20of%20Clans&packageName=com.mattmattica.clash&dependencies=lombok,devtools,web,thymeleaf,data-jpa,h2,mysql,validation,actuator

---
+++ src/test/java/com/mattmattica/clash/ClanCapitalProgressTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mattmattica.clash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClanCapitalProgressTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
